ba608dc610da151ea42cc41a8dea38f51855be30
Jason Wang <1724555125@qq.com>
2021-04-08 21:03:47 +0800

    No need to clean

--- build-x-tools.sh
+++ build-x-tools.sh
@@ -3,7 +3,6 @@
 ct_ng_package="${1:-crosstool-ng-1.24.0}"
 for config in /x-tools-build/ct-ng-actions/configs/${ct_ng_package}/*.config; do
     cp $config .config
-    ./ct-ng clean
     ./ct-ng build
 done
 
